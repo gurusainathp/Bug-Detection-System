aa768a1e19d82c03da63a5572a5dfdd202d91e71
gurusainathp <144239466+gurusainathp@users.noreply.github.com>
2024-08-25 14:35:37 +0530

    Update TesterDAL.java

--- src/main/java/com/hsbc/storage/TesterDAL.java
+++ src/main/java/com/hsbc/storage/TesterDAL.java
@@ -3,10 +3,11 @@ package com.hsbc.storage;
 import com.hsbc.exceptions.BugNotFoundException;
 import com.hsbc.models.BugSeverity;
 import com.hsbc.models.Project;
+import com.hsbc.models.User;
 
 public interface TesterDAL {
 
-    void raiseBug(int bugId, String bugMessage, BugSeverity bugSeverity, Project project);
+    void raiseBug(int bugId, String bugMessage, BugSeverity bugSeverity, Project project, User assignedDeveloper);
 
     void inspectBug(int bugId) throws BugNotFoundException;
 
